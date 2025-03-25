674c5b44219f758d8d55106186b71a25ee1ad580
Mohamed Karim <mohamedkarim909018@gmail.com>
2025-03-25 16:12:34 +0200

    Merging latest changes before pushing

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  images: {
+    logoDark: require('./images/trello.png'),
+    appIcon: require('./images/trello_name.png')
+  },
+  colors: {
+    secondary: '#00c2e0'
+  }
+}
+
+export default assets
